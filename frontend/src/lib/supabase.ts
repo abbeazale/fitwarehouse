a50e44866b71b8f26b8785bf1dd8a6c50014899e
Abbe Azale <abbeazale98@gmail.com>
2025-12-02 13:54:32 -0800

    clean up project

--- frontend/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ikbpmyqftmlgyvfysucs.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrYnBteXFmdG1sZ3l2ZnlzdWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4NzQyNjEsImV4cCI6MjA3OTQ1MDI2MX0.jvZwCyyd5UhzLyLnVAWB2tr7JG5cDAtTKnGch-14xFM'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
